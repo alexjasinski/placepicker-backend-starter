34d0e311a9b8a368374f6584fce6f474ad9fb02a
Alexander Jasinski <design.jasinski@gmail.com>
2024-01-02 19:37:17 +0100

    initial build

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,41 +3,42 @@ import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 import Error from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from '../http.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [error, setError ] = useState();
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
 
-        try {
-          const response = await fetch('http://localhost:3000/places');
-          const resData = await response.json();
-    
-          if(!response.ok) {
-            throw new Error('Failed to fetch places');
-          }
+      try {
+        const places = await fetchAvailablePlaces();
 
-          navigator.geolocation.getCurrentPosition((position) => {
-            const sortedPlaces = sortPlacesByDistance(resData.places, position.coords.latitude, position.coords.longitude);
-            setAvailablePlaces(sortedPlaces);
-          });
-
-        } catch (error) {
-          setError({message: error.message} || "Could not fetch places. Please try again later!");
-        }
-
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
           setAvailablePlaces(sortedPlaces);
+        });
+      } catch (error) {
+        setError(
+          { message: error.message } ||
+            'Could not fetch places. Please try again later!'
+        );
+      }
+
       setIsFetching(false);
     }
     fetchPlaces();
   }, []);
 
-  if(error) {
-  return <Error title="An error occured" message={error.message} />
+  if (error) {
+    return <Error title="An error occured" message={error.message} />;
   }
 
   return (
@@ -50,4 +51,4 @@ export default function AvailablePlaces({ onSelectPlace }) {
       onSelectPlace={onSelectPlace}
     />
   );
-  }
+}
